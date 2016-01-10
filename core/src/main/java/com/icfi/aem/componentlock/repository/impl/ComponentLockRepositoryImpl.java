bd35b1fd3d13c264caf37bacba47890bb9e13f88
SnyderGP <gsnyder@citytechinc.com>
2016-01-09 22:37:33 -0600

    Adding cache for component lock repository

--- core/src/main/java/com/icfi/aem/componentlock/repository/impl/ComponentLockRepositoryImpl.java
+++ core/src/main/java/com/icfi/aem/componentlock/repository/impl/ComponentLockRepositoryImpl.java
@@ -2,19 +2,22 @@ package com.icfi.aem.componentlock.repository.impl;
 
 import com.icfi.aem.componentlock.constants.JcrProperties;
 import com.icfi.aem.componentlock.constants.Paths;
-import com.icfi.aem.componentlock.repository.ComponentLockRepository;
 import com.icfi.aem.componentlock.model.LockPermission;
+import com.icfi.aem.componentlock.repository.ComponentLockRepository;
 import org.apache.sling.api.resource.Resource;
 import org.apache.sling.api.resource.ResourceResolver;
 import org.apache.sling.api.resource.ResourceUtil;
 import org.apache.sling.models.annotations.Model;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Model(adaptables = ResourceResolver.class)
 public class ComponentLockRepositoryImpl implements ComponentLockRepository {
 
     private final ResourceResolver resolver;
+    private final Map<String, LockPermission> cache = new HashMap<>();
 
     public ComponentLockRepositoryImpl(ResourceResolver resolver) {
         this.resolver = resolver;
@@ -47,7 +50,10 @@ public class ComponentLockRepositoryImpl implements ComponentLockRepository {
     }
 
     private LockPermission checkResourceType(String resourceType, List<String> principalIds) {
-        LockPermission out = LockPermission.DEFAULT;
+        LockPermission out = cache.get(resourceType);
+        if (out != null) {
+            return out;
+        }
         for (String principalId : principalIds) {
             String path = Paths.COMPONENT_LOCK_ROOT + "/" + principalId;
             path = resourceType != null ? path + "/" + resourceType : path;
@@ -55,13 +61,14 @@ public class ComponentLockRepositoryImpl implements ComponentLockRepository {
             if (resource != null && !ResourceUtil.isNonExistingResource(resource)) {
                 String permission =
                     resource.getValueMap().get(JcrProperties.CL_PERMISSION, LockPermission.DEFAULT.name());
-                LockPermission lockPermission = LockPermission.fromName(permission);
-                out = lockPermission != null ? lockPermission : LockPermission.DEFAULT;
-                if (out != LockPermission.DEFAULT) {
-                    return out;
+                out = LockPermission.fromName(permission);
+                if (out != null && out != LockPermission.DEFAULT) {
+                    break;
                 }
             }
         }
+        out = out == null ? LockPermission.DEFAULT : out;
+        cache.put(resourceType, out);
         return out;
     }
 }
